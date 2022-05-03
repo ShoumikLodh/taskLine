0330c660a46c472322a1bf9de39eb8ade02df23b
alvinjoseph7 <alvin9joseph@gmail.com>
2022-05-03 17:38:10 +0530

    change course box html semantics

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -1,47 +1,14 @@
-//#region 
-/* document.addEventListener('DOMContentLoaded', function() {
-	// let signInButton = document.querySelector('#sign-in-button');
-	// signInButton.onc = () => {
-			// signInButton.onclick = handleAuthClick;
-
-			authenticate()
-				.then(loadClient).then(execute)
-			.then(taskList => {
-				showCourseView(taskList);
-			});
-		// } 
-}); */
-
-// var one_off = false;
-/* 
-try {
-localStorage.setItem("login", 0);
-}
-catch(err) {
-	console.error(err);
-} */
-//#endregion
 
 window.onload = () => {
-	// init();
 	authenticate().then(loadClient).then(execute);
 	
 }
-	// .then(execute)
-	// .then(taskList => {
-	// });
 
-
-/* async function init() {
-	await authenticate().then(loadClient);
-	execute();
-} */
-
-// try to make this function async
 async function execute() {
 	var taskList = {};
+	let
 	try {
-		let response = await gapi.client.classroom.courses.list({
+		const response = await gapi.client.classroom.courses.list({
 			"courseStates": [
 				"ACTIVE"
 			]
@@ -53,6 +20,7 @@ async function execute() {
 			.then(taskListWithRange => {
 				taskList[course.id] = taskListWithRange;
 				addInstructorInfo(course.ownerId, course_div);
+				// addCourseBoxItem(course, course_div);
 	
 			})
 			.catch(err => {
@@ -70,57 +38,55 @@ async function execute() {
 
 }
 
-		// .then(function(response) {
-			// response.result.courses.forEach(course => {
-			// 	let course_div = addCourseBoxItem(course);
-			// 	fetchCourseWork(course, course_div)
-			// 	.then(taskListWithRange => {
-			// 		taskList[course.id] = taskListWithRange;
-			// 		addInstructorInfo(course.ownerId, course_div);
-
-			// 	})
-			// 	.catch(err => {
-			// 		console.log(err.message);
-			// 		document.body.removeChild(course_div);
-			// 	}); 
-			// });
-			// return taskList;
-		// },
-		// function(err) { console.error("Execute error", err);}
-		// );
 
 
 function addCourseBoxItem(course) {
 	let course_div = document.createElement("div");
-	course_div.className = "course-title-box";
 	course_div.style.display = "none";
-
-	let course_title = document.createElement("h3");
-	course_title.className = "course-title-item";
-	course_title.innerHTML = course.name;
+	course_div.className = "course-title-box";
+	course_div.innerHTML = `
+	<h3 class = "course-title-item">${course.name}</h3>`;
+	// let course_title = document.createElement("h3");
+	// course_title.className = "course-title-item";
+	// course_title.innerHTML = course.name;
+	// course_div.appendChild(course_title);
 
 
 	document.body.appendChild(course_div);
-	course_div.appendChild(course_title);
 	return course_div;
 }
 
 function addInstructorInfo(teacher_id, courseDiv) {
-	return gapi.client.classroom.userProfiles.get(
-		{
-			'userId': teacher_id
-		})
-		.then(function(response) {
-			let profName = document.createElement('p');
-			profName.className = "course-prof-item";
-			profName.innerHTML = response.result.name.fullName;
-			courseDiv.insertBefore(profName, courseDiv.childNodes[1]);
-			
-				createProfPic(response, courseDiv);
-		});
+	return gapi.client.classroom.userProfiles.get({ 'userId': teacher_id})
+		
+	.then(function(response) {
+		let profName = response.result.name.fullName;
+		courseDiv.innerHTML += `<p class="course-prof-item">${profName}<p>`;
+		
+		let profPic = document.createElement("img");
+		profPic.className = "course-prof-pic-item";
+		profPic.src = "https:".concat(response.result.photoUrl);
+		profPic.referrerPolicy = "no-referrer";
+
+		profPic.onload = () => {
+			courseDiv.insertBefore(profPic, courseDiv.firstChild);
+			courseDiv.style.display = "block";
+			// courseDiv.style.border = "1px red solid";
+		}
+		// let picUrl = "https:".concat(response.result.photoUrl);
+		// profPic.style.height = '30px';
+		// profPic.style.width = '30px';
+		
+		// let profName = document.createElement('p');
+		// profName.className = "course-prof-item";
+		// profName.innerHTML = response.result.name.fullName;
+		// courseDiv.insertBefore(profName, courseDiv.childNodes[1]);
+		
+			// createProfPic(response, courseDiv);
+	});
 }
 	
-function createProfPic(response, courseDiv) {
+/* function createProfPic(response, courseDiv) {
 	let profPic = document.createElement("img");
 	profPic.className = "course-prof-pic-item";
 	profPic.style.height = '30px';
@@ -136,6 +102,6 @@ function createProfPic(response, courseDiv) {
 	
 } 
 
- 
+  */
 	
 	
